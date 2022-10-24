5ab6d1522fd8e6aed299b91af6fba7677cc313ce
Matthew Rathkey <rathkeym@gmail.com>
2022-10-23 23:36:09 -0600

    Complete e3-3.c
    Completed the first draft of e3-3.c

--- e3-3.c
+++ e3-3.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX 100
+#define MAX 500
+
+/* Finds integer x in sorted integer array v[] */
+/* Didn't account for input error */
 
 int binsearch(int x, int *v, int n);
 
 int main(void)
 {
-    int x, n;
-    int *v;
+    int x, pos;
+    int n = 100;
+    int *v = malloc(MAX*sizeof(int));
 
-    for(int i=0; i<MAX; i++)
+    for(int i=0; i<n; i++)
     {
-        
+        v[i] = i;
     }
+
+    printf("Integer: ");
+    scanf("%d", &x);
+
+    pos = binsearch(x, v, n);
+
+    if(pos!=(-1)) printf("Integer found at position %d\n", pos);
+    else printf("Integer not found\n");
+
+    free(v);
+    return 0;
 }
 
 int binsearch(int x, int *v, int n)
@@ -24,7 +40,7 @@ int binsearch(int x, int *v, int n)
     while(low<=high)
     {
         mid = (low+high)/2;
-        if(x<v[mid]);
+        if(x<v[mid])
         {
             high = mid - 1;
         }
