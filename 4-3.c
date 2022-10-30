c941bf5f7eb9040439ea32968b02a8684d3e3f62
Matthew Rathkey <rathkeym@gmail.com>
2022-10-30 00:56:26 -0600

    Finish 4-3.c
    Added negative number functionality in getop(). Added a comment
    regarding %, as I'm not sure if it's actually illegal to mod by 0.

--- 4-3.c
+++ 4-3.c
@@ -14,12 +14,14 @@ Input must be entered with spaces separating digits. Operator appears after nece
 e.g. "4 5 +" equals 9 */
 /* Add modulus operator */
 /* Add negative numbers */
+/* Largely assumes valid input */
 
 int main(void)
 {
-    int type;
+    int type, intconvert;
     double op2;
     char *s = malloc(MAXOP*sizeof(char));
+    intconvert = 0; // Warns user of conversion to integer operands at any point in calculation
 
     while((type = getop(s)) != EOF)
     {
@@ -55,14 +57,22 @@ int main(void)
                 if(op2!=0.0)
                 {
                     push(((int) pop()) % ((int) op2));
+                    intconvert = 1;
                 }
                 else
                 {
                     printf("error: zero divisor\n");
+                    /* Perhaps not actually an error, depending on how
+                    mod is defined */
                 }
                 break;
             case '\n':
                 printf("\t%.8g\n", pop());
+                if(intconvert)
+                {
+                    printf("\tWarning: decimal truncation may have occurred\n");
+                    intconvert = 0;
+                }
                 break;
             default:
                 printf("error: unknown command %s\n", s);
@@ -109,19 +119,30 @@ void ungetch(int);
 /* getop: get next operator or numeric operand */
 int getop(char *s)
 {
-    int i, c;
+    int i, c, c_spare;
 
     while((s[0] = c = getch()) == ' ' || c == '\t')
     {
         ;
     }
-
     s[1] = '\0';
-    if(!isdigit(c) && c!='.') // Not a number
+
+    if(!isdigit(c) && c!='.' && c!='-') // Not a number
     {
         return c;
     }
     i = 0;
+
+    if(c=='-') // Check to see if '-' denotes a negative number or a minus sign
+    {
+        s[1] = c_spare = getch();
+        ungetch(c_spare);
+        if(!isdigit(c_spare))
+        {
+            return c;
+        }
+        c = c_spare;
+    }
 
     if(isdigit(c)) // Collect integer part
     {
