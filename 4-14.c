a5fcf8bbf8aae53d76617afe5a6e5121769b93cb
Matthew Rathkey <rathkeym@gmail.com>
2022-11-08 23:16:24 -0700

    Add additional comment to 4-14.c

--- 4-14.c
+++ 4-14.c
@@ -3,6 +3,7 @@
 // Define a macro swap(t, x, y) that interchanges two arguments of type t
 // Seems like I use #define and then write code?
 // Need to get this done tomorrow :)
+// I'm excited for chapter 5
 
 int main(void)
 {
