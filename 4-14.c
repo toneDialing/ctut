9eba7f863364ea27bcaf8a082ddcdfc9678f8a67
Matthew Rathkey <rathkeym@gmail.com>
2022-11-08 23:14:07 -0700

    Add timeline for 4-14.c

--- 4-14.c
+++ 4-14.c
@@ -2,6 +2,7 @@
 
 // Define a macro swap(t, x, y) that interchanges two arguments of type t
 // Seems like I use #define and then write code?
+// Need to get this done tomorrow :)
 
 int main(void)
 {
