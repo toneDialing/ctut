0f730a7c9bdee27538a0ef83241f32ab15ec5037
Matthew Rathkey <rathkeym@gmail.com>
2022-11-09 12:59:52 -0700

    Finish 4-14.c

--- 4-14.c
+++ 4-14.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 
-// Define a macro swap(t, x, y) that interchanges two arguments of type t
-// Seems like I use #define and then write code?
-// Need to get this done tomorrow :)
-// I'm excited for chapter 5
+// Defines a macro swap(t, x, y) that interchanges two arguments of type t
 
-// I suspect main() will be largely blank and most coding will be done with #define
+#define swap(t, x, y)   t temp = x; \
+                        x = y; \
+                        y = temp
+// Note absence of terminating semicolon in #define to allow for presence of semicolon in code
+/* Techinically this isn't necessary as a duplicated ; simply creates an empty statement,
+    but it's slightly cleaner */
 
+/* Hard-coded for simplicity */
 int main(void)
 {
-    
+    int x = 5;
+    int y = 6;
+
+    swap(int, x, y);
+
+    printf("x: %d\ny: %d\n", x, y);
+
+    return 0;
 }
