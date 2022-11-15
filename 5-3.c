ff44234dc499b15e00259b590001858a813a6eb6
Matthew Rathkey <rathkeym@gmail.com>
2022-11-14 17:05:36 -0700

    Finish 5-3.c

--- 5-3.c
+++ 5-3.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void strcat(char *s, char *t);
+#define MAX_LENGTH 1000
+
+void string_concat(char *s, char *t);
 /* Concatenate t to end of s; s must be big enough */
+/* Assumes strings are smaller than MAX_LENGTH */
 
 int main(void)
 {
+    char *s = malloc(MAX_LENGTH*sizeof(char));
+    char *t = malloc(MAX_LENGTH*sizeof(char));
+    int c, i;
+    i = 0;
 
+    printf("Enter first string: ");
+    while((c=getchar())!='\n' && c!=EOF && i<(MAX_LENGTH-1))
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    i = 0;
+
+    printf("Enter second string: ");
+    while((c=getchar())!='\n' && c!=EOF && i<(MAX_LENGTH-1))
+    {
+        t[i++] = c;
+    }
+    t[i] = '\0';
+
+    string_concat(s, t);
+    printf("New concatenated string: %s\n", s);
+
+    free(s);
+    free(t);
+    return 0;
 }
 
-strcat(char *s, char *t)
+void string_concat(char *s, char *t)
 {
-
+    while(*s++)
+        ;
+    s--;
+    while((*s++ = *t++))
+        ;
 }
