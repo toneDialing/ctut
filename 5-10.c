9d1763793db6d36e92d12fc1a01d4d796e6f6b4a
Matthew Rathkey <rathkeym@gmail.com>
2022-11-19 23:53:40 -0700

    Create 5-10.c

---
+++ 5-10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Create a reverse Polish calculator that functions entirely within the command line */
+/* Don't need to store variables or do advanced operations */
+
+int main(void)
+{
+
+}
