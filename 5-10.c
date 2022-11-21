ae8163446943017b2fac5a3fb97335390ef7572a
Matthew Rathkey <rathkeym@gmail.com>
2022-11-20 23:42:31 -0700

    Add comment to 5-10.c

--- 5-10.c
+++ 5-10.c
@@ -3,6 +3,8 @@
 /* Create a reverse Polish calculator that functions entirely within the command line */
 /* Don't need to store variables or do advanced operations */
 
+/* Pull code from previous Polish calculator */
+
 int main(void)
 {
 
