4d63e10d35a5c0c469f6d6c4f2ae7571279ac81c
Matthew Rathkey <rathkeym@gmail.com>
2022-11-22 23:34:55 -0700

    Add args to 5-10.c
    Added initial command-line arg scaffolding to 5-10.c

--- 5-10.c
+++ 5-10.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /* Create a reverse Polish calculator that functions entirely within the command line */
 /* Don't need to store variables or do advanced operations */
 
 /* Pull code from previous Polish calculator */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-
+    if(argc<4)
+    {
+        printf("Insufficient operands\n");
+        return 0;
+    }
 }
