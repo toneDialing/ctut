a6c97cd73133e3a8dff07c714e33dacdd88c78d7
Matthew Rathkey <rathkeym@gmail.com>
2022-11-23 23:58:14 -0700

    Complete draft of 5-10.c

--- 5-10.c
+++ 5-10.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 /* Create a reverse Polish calculator that functions entirely within the command line */
@@ -12,11 +13,67 @@ double pop(void);
 
 int main(int argc, char *argv[])
 {
-    if(argc<4)
+    int c;
+    double op2, answer;
+
+    for(int i = 1; i<argc; i++)
     {
-        printf("Insufficient operands\n");
-        return 0;
+        c=*argv[i];
+        if(!isdigit(c) && c != '.' && c != '-')
+        {
+            switch(c)
+            {
+                case ' ':
+                    break;
+                case '\t':
+                    break;
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '/':
+                    op2 = pop();
+                    if(op2!=0.0)
+                    {
+                        push(pop() / op2);
+                    }
+                    else
+                    {
+                        printf("error: zero divisor\n");
+                    }
+                    break;
+                default:
+                    printf("error: unknown command %c\n", s[i]);
+                    break;
+            }
+        }
+        else
+        {
+            if(c=='-')
+            {
+                if(!*++argv[i])
+                {
+                    op2 = pop();
+                    push(pop() - op2);
+                }
+                else
+                {
+                    push(atof(*argv[i]));
+                }
+            }
+            else
+            {
+                push(atof(*argv[i]));
+            }
+        }
     }
+
+    answer = pop();
+    printf("\t%.8g\n", answer);
+
+    return 0;
 }
 
 #define MAXVAL 100
@@ -24,12 +81,26 @@ int main(int argc, char *argv[])
 int stack_position = 0;
 double stack[MAXVAL];
 
+/* push: push f onto value stack */
 void push(double f)
 {
-
+    if(stack_position<MAXVAL)
+    {
+        stack[stack_position++] = f;
+    }
+    else printf("error: stack full, can't push %g\n", f);
 }
 
+/* pop: pop and return top value from stack */
 double pop(void)
 {
-
+    if(stack_position>0)
+    {
+        return stack[--stack_position];
+    }
+    else
+    {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
 }
