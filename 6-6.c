55adec0c1d80b014f00724b72d9dc36ec4327736
Matthew Rathkey <rathkeym@gmail.com>
2022-12-22 23:38:54 -0700

    Create 6-6.c

---
+++ 6-6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Implement a simple version of the #define processor (i.e., no arguments) suitable for use with C programs,
+    based on the routines of this section. You may also find getch() and ungetch() helpful. */
+
+int main(void)
+{
+
+}
