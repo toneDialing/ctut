4c95d1cf5a6594e294650cc7147557f29ac35c39
Matthew Rathkey <rathkeym@gmail.com>
2022-12-23 23:55:02 -0700

    Sketch code for 6-6.c

--- 6-6.c
+++ 6-6.c
@@ -3,6 +3,10 @@
 /* Implement a simple version of the #define processor (i.e., no arguments) suitable for use with C programs,
     based on the routines of this section. You may also find getch() and ungetch() helpful. */
 
+/* All the necessary hashmap functions are already written as install() and lookup() etc.,
+    so I should only need to use getch()/ungetch() to search for '#'s followed by "define" */
+/* Use if for '#' then loop along to match chars to "define" */
+
 int main(void)
 {
 
