c80941deacb55bb114510badf0fb80155eaa7205
Matthew Rathkey <rathkeym@gmail.com>
2023-01-20 23:56:39 -0700

    Create 7-1.c

---
+++ 7-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Write a program that converts upper case to lower or lower case to upper, depending on the name
+    it is invoked with, as found in argv[0] */
+
+int main(void)
+{
+    
+}
