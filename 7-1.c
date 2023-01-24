623aaa78a6a4ac69f9049bb86dbcc464219247f2
Matthew Rathkey <rathkeym@gmail.com>
2023-01-23 23:46:21 -0700

    Add plan for 7-1.c

--- 7-1.c
+++ 7-1.c
@@ -3,6 +3,11 @@
 /* Write a program that converts upper case to lower or lower case to upper, depending on the name
     it is invoked with, as found in argv[0] */
 
+/* To do:
+    Review argc and argv
+    Otherwise just use isupper() and islower()
+    Should be simple enough; it only requires a pointer to a function */
+
 int main(void)
 {
     
