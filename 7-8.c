cd8ee5718541183244d03fa792138c6eac41de63
Matthew Rathkey <rathkeym@gmail.com>
2023-01-29 23:23:15 -0700

    Create 7-8.c

---
+++ 7-8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* Write a program to print a set of files, starting each new one on a new page, with a title and
+    a running page count for each file */
+
+int main(void)
+{
+
+}
