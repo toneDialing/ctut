cefa7da492f8ef1775e3be354cf674c3189df1b0
Matthew Rathkey <rathkeym@gmail.com>
2023-02-01 00:15:59 -0700

    Polish 7-1.c and update .gitignore

--- 7-1.c
+++ 7-1.c
@@ -5,19 +5,15 @@
 /* Write a program that converts upper case to lower or lower case to upper, depending on the name
     it is invoked with, as found in argv[0] */
 
-/* To do:
-    Review argc and argv
-    Otherwise just use isupper() and islower()
-    Should be simple enough; it only requires a pointer to a function */
+/* This could also have been written using a pointer to a function, but this seems clear enough */
 
 int main(int argc, char *argv[])
 {
     int c;
-    int (*case_change)(int);
 
     if(argc!=2)
     {
-        printf("command line error\n");
+        printf("command line error: type \"lower\" or \"upper\"\n");
         return 0;
     }
 
@@ -35,7 +31,7 @@ int main(int argc, char *argv[])
             putchar(toupper(c));
         }
     }
-    else printf("command line error\n");
+    else printf("command line error: type \"lower\" or \"upper\"\n");
 
     return 0;
 }
