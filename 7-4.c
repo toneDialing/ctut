b6a77e491e1c396734043b13af7f1b24914c1109
Matthew Rathkey <rathkeym@gmail.com>
2023-02-04 02:01:22 -0700

    Write first parts of 7-4.c
    Added skipping blanks/tabs and parsing literal characters to minscanf().

--- 7-4.c
+++ 7-4.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 /* Write a private version of scanf() analogous to minprintf() from the previous section */
 
+int minscanf(char *, ...);
+
 int main(void)
 {
+    int n;
 
+    minscanf("%d", &n);
+    printf("%d\n", n);
+
+    return 0;
+}
+
+int minscanf(char *fmt, ...)
+{
+    va_list ap;
+    char *p;
+    int c;
+
+    va_start(ap, fmt);
+    for(p = fmt; *p; p++)
+    {
+        if(*p==' ' || *p=='\t') /* skip blanks and tabs */
+        {
+            continue;
+        }
+        if(*p!='%') /* literal characters */
+        {
+            if((c = getchar())!=*p)
+            {
+                printf("Error: incorrect format for scanf()\n");
+                return 0;
+            }
+        }
+    }
+    va_end(ap);
+    /* add return value */
 }
