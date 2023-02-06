e1fc2a0a7179aeeb3396bd9b797abf1802b4c2e2
Matthew Rathkey <rathkeym@gmail.com>
2023-02-05 22:58:56 -0700

    Fixed error message in 7-7.c

--- 7-7.c
+++ 7-7.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 
     if(argc==1)
     {
-        printf("Usage: 7-7.c [pattern] [opt. file names]\n");
+        printf("Usage: ./7-7 [pattern] [opt. file names]\n");
         return 0;
     }
 
